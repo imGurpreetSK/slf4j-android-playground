5229bb0cfd2bac1e41a2d253c340e12a36ec13e4
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-08-21 00:14:59 +0530

    Add async appender backed by rolling file appender.

--- app/src/main/java/com/gurpreetsk/test/LogApp.kt
+++ app/src/main/java/com/gurpreetsk/test/LogApp.kt
@@ -1,9 +1,11 @@
 package com.gurpreetsk.test
 
 import android.app.Application
-import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.*
+import org.apache.log4j.varia.LevelRangeFilter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.File
 
 class LogApplication : Application() {
 
@@ -12,9 +14,40 @@ class LogApplication : Application() {
     }
 
     override fun onCreate() {
-        BasicConfigurator.configure()
-        logger.info("Before onCreate()")
+        configureLogger()
+
+        logger.info("Before Application onCreate()")
         super.onCreate()
-        logger.info("After onCreate()")
+        logger.info("After Application onCreate()")
+    }
+
+    private fun configureLogger() {
+        val logsDir = File(filesDir, "logs")
+        if (logsDir.mkdir()) {
+            File(logsDir, "test.txt").apply {
+                createNewFile()
+                setWritable(true)
+                setReadable(true)
+            }
+        }
+
+        val logPattern = "%p    %r [%t] %c %x - %m%n"
+        BasicConfigurator.configure(
+            AsyncAppender().also {
+                it.bufferSize = 64
+                it.addAppender(
+                    RollingFileAppender(
+                        PatternLayout(logPattern),
+                        "${filesDir}${File.separator}logs${File.separator}test.txt"
+                    ).apply {
+                        maximumFileSize = 10 * 1024 * 1024 // 2 MB
+                        addFilter(LevelRangeFilter().apply {
+                            levelMax = Level.FATAL
+                            levelMin = Level.INFO
+                        })
+                    }
+                )
+            }
+        )
     }
 }
--- app/src/main/java/com/gurpreetsk/test/MainActivity.kt
+++ app/src/main/java/com/gurpreetsk/test/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.gurpreetsk.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        LogApplication.logger.info("Before onCreate()")
+        LogApplication.logger.info("Before Activity onCreate()")
         super.onCreate(savedInstanceState)
-        LogApplication.logger.info("After onCreate()")
+        LogApplication.logger.info("After Activity onCreate()")
         setContentView(R.layout.activity_main)
+
+        (0..Long.MAX_VALUE).forEach {
+            LogApplication.logger.error("YOLO $it")
+        }
     }
 }
